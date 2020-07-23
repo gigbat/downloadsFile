384b4bcb9946102ca5319eb69d53ef958320026c
bogdan <b.kostevich@gmail.com>
2020-07-23 22:37:16 +0300

    New Class Download

---
+++ src/Download.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.URL;
+
+// @links https://code.jquery.com/jquery-3.5.1.min.js
+
+public class Download {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    final String FILE_URL;
+
+    public Download() throws IOException {
+        FILE_URL = bf.readLine();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Download download = new Download();
+        download.download();
+    }
+
+    public void download() {
+        try (BufferedInputStream bfDownload = new BufferedInputStream(new URL(FILE_URL).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("file")) {
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((count = bfDownload.read(buffer, 0, 1024)) != -1) {
+                fileOutputStream.write(buffer, 0, count);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
